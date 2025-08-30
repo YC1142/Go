b54868c915e26ba02dcb95d1f51225f11e85e1d1
YC1142 <nkim6483@web.de>
2025-08-30 22:25:03 +0200

    30.08.2025 (1)

--- Practice/01_Hello.go
+++ Practice/01_Hello.go
@@ -2,12 +2,11 @@ package main
 
 import "fmt"
 
-func main(){
+func main() {
 	fmt.Println("Hello world!")
 	number := 50
 	fmt.Println(number)
 	const constant_value = 129
 	print(constant_value)
-
-
+	
 }
